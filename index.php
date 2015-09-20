cdda2200fc4e623df70e8be9aebd27dc4f8f96e9
Denisof <dloschakov@gmail.com>
2015-09-20 23:59:49 +0300

    Basic application

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php 
+error_reporting(E_ALL);
+use Symfony\Component\Console\Application;
+require __Dir__ . '/vendor/autoload.php';
+$app=new Application();
+$app->run();
 echo 'Hello world';
 
 ?>
